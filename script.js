d48d755c9d3056582725afc85e30c035e1eab6ff
Fikayo <Fikayoz@gmail.com>
2026-01-08 22:02:16 +0900

    Completed desired functionalities

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-let firstVar = 1;
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause")
+        ctrlIcon.classList.add("fa-play")
+    }
+    else{
+        song.play();
+        ctrlIcon.classList.remove("fa-play")
+        ctrlIcon.classList.add("fa-pause")
+    }
+}
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    }, 500)
+}
+
+progress.onchange = function(){
+    song.currentTime = progress.value;
+    song.play(); // Remove this and the next two lines if you don't want it to auto-play on touching the progress bar
+    ctrlIcon.classList.remove("fa-play")
+    ctrlIcon.classList.add("fa-pause")
+}
